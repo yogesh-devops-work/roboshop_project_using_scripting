c67ad74ea9a2b189d77a7f0b986d41106c80aba6
Yoges Kumar <y.singh03@gmail.com>
2024-10-23 17:41:08 +0530

    roboshop- frontend-nginx

---
+++ mongodb.sh
@@ -0,0 +1,12 @@
+script_location=$(pwd)
+
+echo "save the repo file on yum repos to install the mongo"
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+echo "Installing mongodb from repo"
+yum install mongodb-org -y
+
+echo "start and enable the mongod service"
+
+systemctl enable mongod
+systemctl start mongod
